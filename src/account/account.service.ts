fec8320b1285fb20be15566bd37539a4545a0a73
Widmar Raul <widmarito@gmail.com>
2024-03-07 15:25:47 -0500

    CRUD for accounts

---
+++ src/account/account.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { CreateAccountDto, UpdateAccountDto } from './dto';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Account } from './schemas/account.schema';
+
+@Injectable()
+export class AccountService {
+  constructor(
+    @InjectModel(Account.name) private accountModel: Model<Account>
+  ) { }
+
+  async create(createAccountDto: CreateAccountDto): Promise<Account> {
+    const account = await this.accountModel.create(createAccountDto);
+    return account;
+  }
+
+  async findAll(): Promise<Account[]> {
+    const accounts = await this.accountModel.find();
+    return accounts;
+  }
+
+  async findOne(id: string): Promise<Account> {
+    const account = await this.accountModel.findById(id);
+    return account;
+  }
+
+  async update(id: string, updateAccountDto: UpdateAccountDto): Promise<Account> {
+    const account = await this.accountModel.findByIdAndUpdate(
+      id,
+      updateAccountDto,
+      { new: true },
+    );
+    return account;
+  }
+
+  async remove(id: string): Promise<Account> {
+    const account = await this.accountModel.findByIdAndDelete(id);
+    return account;
+  }
+}
